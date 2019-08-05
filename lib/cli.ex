29f3aa98c92148db32df52d27e2f90555855391b
Axel Gschaider <axel.gschaider@posteo.de>
2019-08-05 21:37:57 +0200

    Set up credo
    Also fixes a missing @moduledoc that credo brought up.

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,7 +1,14 @@
 
 defmodule Trakki.CLI do
+  @moduledoc """
+  The base module needed as entrypoint for escript.
+  
+  Currently not really fleshed out. Just prints a message.
+  """
+
   def main(_args) do
     IO.puts("This is the captain speaking. We're not propperly configured.")
   end
+
 end
 
